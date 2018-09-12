e0109992189ecc84f4a747bc8edac1ba11b0203f
thimi0412 <thimi0412@icloud.com>
2018-09-12 14:58:34 +0900

    add
    実装のまとめ

---
+++ 3/exercises2.py
@@ -0,0 +1,39 @@
+import numpy as np
+from exercise1 import sigmoid
+
+def indentity_function(x):
+    return x
+
+
+if __name__ == '__main__':
+    '''
+    1層目
+    '''
+    X = np.array([1.0, 0.5])
+    W1 = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])
+    B1 = np.array([0.1, 0.2, 0.3])
+    A1 = np.dot(X, W1) + B1
+    print('A1:{}'.format(A1))
+
+    Z1 = sigmoid(A1)
+    print('Z1:{}'.format(Z1))
+
+    '''
+    2層目
+    '''
+    W2 = np.array([[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]])
+    B2 = np.array([0.1, 0.2])
+    A2 = np.dot(Z1, W2) + B2
+    print('A2:{}'.format(A2))
+
+    Z2 = sigmoid(A2)
+    print('Z2:{}'.format(Z2))
+
+    '''
+    出力層
+    '''
+    W3 = np.array([[0.1, 0.3], [0.2, 0.4]])
+    B3 = np.array([0.1, 0.2])
+    A3 = np.dot(Z2, W3) + B3
+    Y = indentity_function(A3)
+    print('Y:{}'.format(Y))
