ec55f01de6e43f026a28a717bcbea54f6036081b
thimi0412 <thimi0412@icloud.com>
2018-09-13 11:18:30 +0900

    add
    画像の表示

--- 3/exercises5.py
+++ 3/exercises5.py
@@ -1,5 +1,20 @@
 from minist import load_mnist
+import numpy as np
+from PIL import Image
 
-(X_train, X_test), (y_train, y_test) = load_mnist(flatten=True, normalize=False)
+def image_show(img):
+    pil_img = Image.fromarray(np.uint8(img))
+    pil_img.show()
 
-print(X_train.shape)
+
+if __name__ == '__main__':
+    (X_train, t_train), (X_test, t_test) = load_mnist(flatten=True, normalize=False)
+    img = X_train[0]
+    label = t_train[0]
+    print(label)
+
+    print(img.shape)
+    img = img.reshape(28, 28)
+    print(img.shape)
+
+    image_show(img)
