7d7ecc16564f30e69d602ff4e6b1bc3574253521
thimi0412 <thimi0412@icloud.com>
2018-09-14 11:39:13 +0900

    add
    バッチ処理に対応

--- 4/exercises1.py
+++ 4/exercises1.py
@@ -12,8 +12,12 @@ def mean_squared_error(y, t):
 交差エントロピー誤差
 '''
 def cross_entropy_error(y, t):
-    delta = 1e-7
-    return -np.sum(t * np.log(y + delta))
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+    
+    batch_size = y.shape[1]
+    return -np.sum(t * np.log(y)) / batch_size
 
 
 if __name__ == '__main__':
